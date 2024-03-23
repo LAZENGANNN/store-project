778d6e694df505164636b83b2413475abe5f0215
LAZENGANNN <BFGdivision1993>
2024-03-23 19:26:07 +0300

    23.03.2024 (update product card)

--- pages/categotyPage.js
+++ pages/categotyPage.js
@@ -1,22 +1,18 @@
 import {getProducts} from "../api/getProducts";
-import styles from "./categoryPage.module.css";
+import styles from "./allProductsPage.module.css";
 import {productCard} from "../components/productCard";
 
 
 
-export const categoryPage = (categoryName) =>{
+export const categoryPage = (category) =>{
     const div = document.createElement('div');
     div.classList.add(styles.container)
-    const container = document.createElement('div');
-    container.classList.add(styles.container)
-
-    getProducts(`products?category=${categoryName}`)
+    getProducts(`products?category=${category}`)
         .then(
             data =>data.data.map(product => div.append(productCard(product)))
         )
         .then(
             data => console.log(data)
         )
-
     return div
 }
