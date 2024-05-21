f7c5f03dea72ca3cd9698c4a24db9b0b55cbceee
LAZENGANNN <BFGdivision1993>
2024-05-21 18:42:11 +0300

    'pre-final'

--- features/AD/ADBanner.js
+++ features/AD/ADBanner.js
@@ -1,29 +1,32 @@
 import {button} from "../../components/button.js";
 import styles from './ADstyle.module.css'
 
-export const getBottomADBanner = () =>{
-    let isShowed = false
 
+
+
+
+export const getBottomADBanner = () =>{
+    const isShowed =localStorage.getItem('isShowed')
+    if(isShowed){
+        return
+    }
 
     const div = document.createElement('div')
     div.classList.add(styles.container)
     div.classList.add(styles.startAnimation)
     setTimeout(()=> div.classList.add(styles.setEndMargin), 3999)
-    // const mainButton = button(null, () => window.location = 'https://www.casino.org/games/free/')
-
-    const mainButton = button(null,  null)
+    const mainButton = button(null, () => window.location = 'https://www.casino.org/games/free/')
     mainButton.classList.add(styles.mainButton)
 
     const cancelDiv = document.createElement('div')
     cancelDiv.classList.add(styles.cancelDiv)
     const cancelButton = button(null, ()=>
     {
-        isShowed = true
         div.classList.add(styles.showed)
+        localStorage.setItem('isShowed', 'true')
 
     })
     cancelButton.classList.add(styles.cancelButton)
-
 
 
 
--- main.js
+++ main.js
@@ -3,16 +3,15 @@ import {getHeader} from "./features/header/header";
 import {getLayout} from "./features/layaout";
 import {getFooter} from "./features/footer/footer";
 import {getNotification} from "./features/notification/notification.js";
-import {getBottomADBanner} from "./features/AD/ADBanner.js";
-// import { getUserForm } from './features/userForm/getUserForm';
+import {getBottomADBanner} from "./features/ad/ADBanner";
 
 
 
 const app = document.querySelector('#app');
 
 app.classList.add('app');
-app.append(getBottomADBanner())
 app.append(getHeader());
+app.append(getBottomADBanner())
 app.append(getLayout());
 app.append(getNotification())
 app.append(getFooter())
